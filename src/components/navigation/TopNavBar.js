a490fdc972f30fc34f024a1f8b33b816da8afe29
sofiane lasoa <sofiane.lasoa.etu@univ-lille.fr>
2022-12-06 11:40:00 +0100

    Update responsive

--- src/components/navigation/TopNavBar.js
+++ src/components/navigation/TopNavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import SearchIcon from '@mui/icons-material/Search';
 
 const TopNavBar = () => {
 
@@ -13,6 +14,7 @@ const TopNavBar = () => {
         <div className="top-nav-bar">
             <input type="text" placeholder="Search..." onChange={routeChange}></input>
             <div className="top-nav-bar-logo"></div>
+            <div className="search-icon" onClick={() => routeChange()}><SearchIcon /></div>
         </div>
     )
 }
